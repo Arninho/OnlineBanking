e7c994d717c591cfa07e88521f6819901e0aaa9a
zolex77 <zoli@oroszhegy.ro>
2015-11-12 00:50:25 +0200

    Revert "Conf"
    This reverts commit f7ebc140b2ba93377544200e72e4c41b1d9436a5.

--- classes/db.php
+++ classes/db.php
@@ -1,29 +1,8 @@
 <?php
-class DB{
-    private static $_instance = null;
-    private $_con, 
-            $_query,
-            $_error = false,
-            $_results,
-            $_count = 0;
-    
-    private function __construct() {
-        try{
-            $this->_con = new mysqli(Config::get('mysql/host'),Config::get('mysql/username'),Config::get('mysql/password'),Config::get('mysql/db'));
-            echo 'Connected';
-        }
-        catch (mysqli_sql_exception $e){
-        die($e->getMessage());
-        }
-    }
-    
-    public static function getInstance(){
-        if(!isset(self::$_instance)){
-            self::$_instance = new DB();
-        }
-        return self::$_instance;
-    }
-    
-    
-}
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
