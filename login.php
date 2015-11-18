fd15d4684b18d4a85436fd35aa7ee9e1c11de0ca
zolex77 <zoli@oroszhegy.ro>
2015-11-18 23:52:15 +0200

    Login/logout

--- login.php
+++ login.php
@@ -1,8 +1,47 @@
 <?php
+require_once 'core/init.php';
+if (Input::exists()) {
+    if (Token::check(Input::get('token'))) {
+        $validate = new Validate();
+        $validation = $validate->check($_POST, array(
+            'username' => array('required' => true),
+            'password' => array('required' => true)
+        ));
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+        if ($validation->passed()) {
+            $user = new User();
+            
+            $remember=(Input::get('remember')==='on')? true:false;
+            $login = $user->login(Input::get('username'), Input::get('password'),$remember);
 
+            if ($login) {
+                echo Redirect::to('index.php');
+            } else {
+                echo '<p>Sikertelen</p> ';
+            }
+        } else {
+            foreach ($validation->errors() as $error) {
+                echo $error, '<br>';
+            }
+        }
+    }
+}
+?>
+<form action="" method="post">
+    <div class="field">
+        <label for="username">Felhasználónév</label>
+        <input type="text" name="username" id="username" autocomplete="off">
+    </div>
+
+    <div class="field">
+        <label for="password">Jelszó</label>
+        <input type="password" name="password" id="password" autocomplete="off">
+    </div>
+    <div class="field">
+        <label for="remember">
+            <input type="checkbox" name="remember" id="remember">Remember me 
+        </label>
+    </div>
+    <input type="hidden" name="token" value="<?php echo Token::generate(); ?>" >
+    <input type="submit" value="Log in">
+</form>
