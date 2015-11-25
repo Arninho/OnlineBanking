08a469d6fec580291b39249b906d61c1c44337fa
zolex77 <zoli@oroszhegy.ro>
2015-11-25 12:51:08 +0200

    jelszocska

--- changepassword.php
+++ changepassword.php
@@ -1,8 +1,70 @@
 <?php
+require_once 'core/init.php';
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$user= new User();
+if(!$user->isLoggedIn()){
+    Redirect::to('index.php');
+}
+if (Input::exists()){
+    if(Token::check(Input::get('token'))){
+        $validate=new Validate();
+        $validation=$validate->check($_POST,array(
+          'password_current'=>array(
+              'required'=>true,
+              'min'=>6
+          ),
+          'password_new'=>array(
+              'required'=>true,
+              'min'=>6
+          ),
+            'password_new_again'=>array(
+              'required'=>true,
+              'min'=>6,
+              'matches'=>'password_new'
+          ),
+            
+        ));
+        if ($validation->passed()){
+            if(Hash::make(Input::get('password_current'),$user->data()->salt)!==$user->data()->password){
+                
+            }else{
+                $salt=  Hash::salt(32);
+                $user->update(array(
+                    'password'=>  Hash::make(Input::get('password_new'),$salt),
+                    'salt'=>$salt
+                ));
+                Session::flash('home','Jelszo megvaltoztatval !');
+                Redirect::to('index.php');
+            }
+        }else{
+            foreach ($validation->errors()as $error){
+                echo $error,'<br>';
+            }
+        }
+    }
+}
+?>
+<form action="" method="post">
+    
+    <div class="field">
+        <label for="password_current">Current password:</label>
+        <input type="password" name="password_current" id="password_current">
+        
+    </div>
+    
+    <div class="field">
+        <label for="password_new">New password:</label>
+        <input type="password" name="password_new" id="password_new">
+        
+    </div>
+
+    <div class="field">
+        <label for="password_new_again">New password again:</label>
+        <input type="password" name="password_new_again" id="password_new_again">
+        
+    </div>
+
+    <input type="submit" value="Change">
+    <input type="hidden" name="token" value="<?php echo Token::generate(); ?>">
+</form>
 
